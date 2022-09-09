ab90896e753d8c9b8665f055b19e9d542a658876
Alexander Carr <alexanderc93@gmail.com>
2022-09-09 12:13:58 -0700

    prefill time slots with data from localstorage, and set up save button to store textarea content into localstorage for later retrieval

--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,15 @@
 let currentHour = parseInt(moment().format("HH"));
 let timeBlocks = $(".time-block");
 
-
-// loop through our time blocks and compare their data-hour attribute to the current hour
+// loop through our time blocks and compare their data-hour attribute to the current hour, and populate them from localstorage
 for (let i = 0; i < timeBlocks.length; i++) {
     const timeBlock = $(timeBlocks[i]);
     const timeBlockHour = timeBlock.attr("data-hour");
     // get the textarea child of this timeblock to apply our style to later
     const timeBlockInput = timeBlock.find(".time-block-input");
+
+    // get saved value from localstorage for this hour and fill the textArea with it
+    const savedValue = localStorage.getItem(timeBlockHour)
  
     // if it's in the past
     if(timeBlockHour < currentHour) {
@@ -23,3 +25,16 @@ for (let i = 0; i < timeBlocks.length; i++) {
         timeBlockInput.addClass("future")
     }
 }
+
+
+// add event listener to each save button
+let saveButtons = $(".save-btn").click(function() {
+    // check which hour was saved via data-hour attr
+    const savedHour = $(this).attr("data-hour");
+    // get value of the textArea to save it
+    const textToSave = $(`#${savedHour}`).val();
+    
+    // save text into localstorage under a key for the saved hour
+    localStorage.setItem(savedHour, textToSave);
+
+})
